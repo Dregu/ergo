9306a986a2a7bd633a9a94bf2262e8e264692e9a
Dregu <dregu@dreg.us>
2026-08-13 22:57:45 +0300

    die

--- src/main.c
+++ src/main.c
@@ -7,15 +7,16 @@ int main(int argc, char* argv[])
 {
     struct state* state = state_init(argc, argv);
     char input[BUFSIZ * 10];
-    do
+    while (true)
     {
-        if (fgets(input, BUFSIZ * 10, stdin))
-        {
-            parse_input(state, input);
-            wl_surface_attach(state->wl_surface, create_buffer(state), 0, 0);
-            wl_surface_damage_buffer(state->wl_surface, 0, 0, INT32_MAX, INT32_MAX);
-            wl_surface_commit(state->wl_surface);
-        }
-    } while (wl_display_dispatch(state->wl_display) != -1);
+        if (fgets(input, BUFSIZ * 10, stdin) == NULL)
+            break;
+        parse_input(state, input);
+        wl_surface_attach(state->wl_surface, create_buffer(state), 0, 0);
+        wl_surface_damage_buffer(state->wl_surface, 0, 0, INT32_MAX, INT32_MAX);
+        wl_surface_commit(state->wl_surface);
+        if (wl_display_dispatch(state->wl_display) == -1)
+            break;
+    }
     return 0;
 }
